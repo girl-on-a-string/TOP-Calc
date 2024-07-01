462323e456d9871568014267227961d1f2a60ef0
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-01 12:24:08 -0500

    created add, subtract, multiply and divide functions and linked them to buttons. Created operate and populate screen functions

---
+++ index.js
@@ -0,0 +1,41 @@
+//populate screen 
+
+function populate () {
+    
+}
+
+//other buttons
+
+//number buttons
+
+//operator buttons
+
+//add 
+
+function add () {
+    console.log("add");
+}
+
+//subtract
+
+function subtract () {
+    console.log("subtract");
+}
+
+//multiply 
+
+function multiply () {
+    console.log("multiply");
+}
+
+//divide
+
+function divide () {
+    console.log("divide");
+}
+
+//operate
+
+function operate () {
+
+}
