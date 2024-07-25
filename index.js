46738637a6a0d8c04fa25164d0f4cb35f576d1bf
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-25 09:18:27 -0500

    set up btn click eventlistener. Console logs btn value

--- index.js
+++ index.js
@@ -4,28 +4,60 @@ const screen = document.getElementById("screen-display");
 const problemDisplay = document.getElementById("problem");
 const solutionDisplay = document.getElementById("solution");
 
-const btn = document.querySelectorAll("btn");
+let btn = document.querySelectorAll(".btn");
+
+//update display
+
+function updateDisplay () {
+    btn = Array.from(btn);
+    btn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            console.log("btn clicked");
+            let input = btn.innerHTML;
+            console.log(input);
+        });
+    });
+}
+
+updateDisplay();
+
+//btn handling
+
+// function btnClick () {
+//     btn = Array.from(btn);
+//     btn.forEach(btn => {
+//         btn.addEventListener("click", () => {
+//             console.log("recieved");
+//         });
+//     });
+// }
+
+// btnClick();
 
 //operate function
 
 function operate (a, b, e) {
-    switch (e.target.innerText) {
+    switch (btn) {
         case ("+"):
             add(a, b);
+            console.log("add");
             break;
         case ("-"):
             subtract(a, b);
+            console.log("subtract");
             break;
         case ("*"):
             multiply(a, b);
+            console.log("multiply");
             break;
         case ("/"):
             divide(a, b);
+            console.log("divide");
             break;
     }
 }
 
-//add function
+//add
 
 function add (a, b) {
     return a + b;
