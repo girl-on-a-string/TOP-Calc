bb35b7398f4eb9255f71ee6b3edc67fbe68790a1
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-31 20:08:03 -0500

    clear function working. Sign reverse created

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ let problemDisplay = document.getElementById("problem");
 let solutionDisplay = document.getElementById("solution");
 
 let value; //stores any inputted value
-let intVal; //stores inputted int value
-let OpVal; //stores inputted operator
-
 let firstIntVal = ""; //stores first inputted number
 let secondIntVal = ""; //stores second inputted number
+let OpVal; //stores inputted operator
+let solutionVal = ""; //stores the solution of the problem
 
 let isFirstVal = false; //status of first val
 let isSecondVal = false; //status of second val
+let isEqualsPressed = false; //status of the equasion
 
 let btn = document.getElementsByClassName("btn"); 
 btn = Array.from(btn); //so I can add an event listener later
@@ -28,7 +28,12 @@ function btnClick (value) {
         console.log(value);
         if (isFirstVal == false) { //if there's no first val store num in firstIntVal
             firstIntVal += value;
-            console.log(`first value: ${firstIntVal}`)
+
+            if (firstIntVal == 0) { //if the first digit in the number is zero don't show it
+
+            }
+
+            console.log(`first value: ${firstIntVal}`);
         } else if (isFirstVal == true && isSecondVal == "") { //if there is a firstIntVal store this val in secondIntVal
             value = 0;
             secondIntVal += value;
@@ -46,10 +51,26 @@ function operate (value) {
         case "AC":
             problemDisplay.innerText = "";
             solutionDisplay.innerText = "";
+
+            firstIntVal = "";
+            secondIntVal = "";
+            OpVal = "";
+
             console.log("clear");
             break;
         case "+/-":
             console.log("reverse");
+
+            let a = value * 2;
+            
+            if (value > 0) {
+                value = value - a;
+                console.log(value);
+            } else if (value < 0) {
+                value = value + a;
+                console.log(value);
+            }
+
             break;
         case "%":
             console.log("percentage");
@@ -71,11 +92,23 @@ function operate (value) {
             break;
         case "=":
             console.log("evaluate");
+            isEqualsPressed = true;
             break;
     }
 }
 
 //section 4
+//display numbers
+
+function display (value) {
+    if (isEqualsPressed == true) { //when equals is pressed display solution in solution in solution space
+        solutionDisplay += value;
+    } else { //if equals hasn't been pressed store problem in problem space
+        problemDisplay += value;
+    }
+}
+
+//section 5
 //initialize, get everything working
 
 function init () {
@@ -83,6 +116,7 @@ function init () {
         btn.addEventListener("click", (e) => {
             value = e.target.innerText
             btnClick(value);
+            display(value);
         });
     });
 }
