0f0329c57770851d0e18e97cfa1595a3888e7362
girl-on-a-string <frenchfries55@protonmail.com>
2024-08-12 18:46:42 -0500

    redoing code. problem display working

--- index.js
+++ index.js
@@ -1,192 +1,111 @@
-//section 1
+//section 1 
 //variables
 
-let genScreen = document.getElementById("screen-display");
 let problemDisplay = document.getElementById("problem");
 let solutionDisplay = document.getElementById("solution");
 
 let value; //stores inputted value of btns 
 let firstIntVal = ""; //stores first inputted number
 let secondIntVal = ""; //stores second inputted number
-let OpVal; //stores inputted operator
+let OpVal = ""; //stores inputted operator
 let solutionVal = ""; //stores the solution of the problem
 
-let isFirstVal = false; //status of first val
-let isSecondVal = false; //status of second val
-let isEqualsPressed = false; //status of the equasion
-let isOpVal = false; //status of the operator
+let isEqualsPressed = false;
 
 let btn = document.getElementsByClassName("btn"); 
 btn = Array.from(btn); //so I can add an event listener later
 
 //section 2
-//btn click handling
+//button click handling
 
 function btnClick (value) {
-    if (isNaN(value)) {
-        isOpVal = true;
-        
-        if (value !== "AC" || value !== "=") {
-            OpVal = value;
-            operate(OpVal);
-        }  
+    if (isNaN(value)) { //if the value is not a number (operator)
+        OpVal = value;
 
-        if (value == "=") {
-            reset();
+        if (value == "=") { 
+            console.log("evaluate");
+            isEqualsPressed = true;
         }
 
-        console.log(`operator clicked: ${OpVal}`);
-    } else {
-        // console.log(`number clicked: ${value}`);
-        if (isFirstVal == false || isOpVal == false) {
-            getFirstValue(value);
-        } 
-
-        if (firstIntVal !== "" && isOpVal == true) {
-            getSecondValue(value);
+        if (value == "AC") { //clear the display
+            console.log("Clear");
         }
+
+        if (value == ".") { //decimal handling
+            console.log("decimal");
+        }
+
+        if (value == "+/-") { // sign conversion handling
+            console.log("reverse");
+        }
+
+        operate(value);
+    } else { //if the number is an integer
+        if (OpVal == "" && secondIntVal == "") {
+            firstIntVal += value;
+        }
+    }
+}
+
+//section 3
+//storing numbers
+
+function numHandling (value) { //handle what happens with decimals
+
+}
+
+function getValue1 (value) {
+
+}
+
+function getValue2 (value) {
+
+}
+
+//section 4
+//operator handling
+
+function operate (value) {
+    switch (value) {
+        case "+":
+            solutionVal = parseFloat(firstIntVal) + parseFloat(secondIntVal);
+            console.log("add");
+            break;
+        case "-":
+            solutionVal = parseFloat(firstIntVal) - parseFloat(secondIntVal);
+            console.log("subtract");
+            break;
+        case "x":
+            solutionVal = parseFloat(firstIntVal) * parseFloat(secondIntVal);
+            console.log("multiply");
+            break;
+        case "/":
+            solutionVal = parseFloat(firstIntVal) / parseFloat(secondIntVal);
+            console.log("divide");
+            break;
     }
 }
 
 //section
-//get values
+//display everything
 
-function getFirstValue (value) {
-    if (isSecondVal == false || OpVal == "") {
-        firstIntVal += value;
-        console.log("getting first value");
-    } else if (isSecondVal == true || value == "AC" || OpVal !== "") {
-        firstIntVal += "";
-        console.log("first value done");
-    }
-
-    console.log(`first value: ${firstIntVal}`);
-}
-
-function getSecondValue (value) {
-    // if (firstIntVal !== "" && isOpVal == true) {
-        isSecondVal = true;
-        secondIntVal += value;
-        console.log(`second value: ${secondIntVal}`);
-    // }
-}
-
-//section 3
-//operator handling, doing the math
-
-function operate (value) {
-    console.log("operating");
-
-    if (firstIntVal == "" || secondIntVal == "" || OpVal == "") {
-        console.log("nothing to evaluate");
-        return;
-    }
-
-    switch (value) {
-        case "+/-":
-            console.log("reverse");
-
-            let a = value * 2;
-            
-            if (value > 0) {
-                value = value - a;
-                console.log(value);
-            } else if (value < 0) {
-                value = value + a;
-                console.log(value);
-            }
-            break;
-        case "%":
-            console.log("percentage");
-            break;
-        case "/":
-            console.log("divide");
-            if (firstIntVal == 0 || secondIntVal == 0) { //handle division by zero errors
-                console.log("error");
-            }
-            solutionVal = parseFloat(firstIntVal) / parseFloat(secondIntVal);
-            break;
-        case "x":
-            console.log("multiply");
-            solutionVal = parseFloat(firstIntVal) * parseFloat(secondIntVal);
-
-            if (value == 0) {
-                solutionVal = 0;
-            }
-
-            break;
-        case "-":
-            console.log("subtract");
-            solutionVal = parseFloat(firstIntVal) - parseFloat(secondIntVal);
-            break;
-        case "+":
-            console.log("add");
-            solutionVal = parseFloat(firstIntVal) + parseFloat(secondIntVal);
-            break;
-        case ".":
-            console.log("decimal");
-            break;
-        case "=":
-            console.log("evaluate");
-            isEqualsPressed = true;
-            break;
-    }
-}
-
-function evaluate () {
-
-}
-
-function clearDisplay () {
-    OpVal = "";
-    firstIntVal = "";
-    secondIntVal = "";
-
-    problemDisplay.innerText = "";
-    solutionDisplay.innerText = "";
-}
-
-//section 4
-//display numbers
-
-function display (value) {
-    if (isEqualsPressed == true) { //when equals is pressed display solution in solution in solution space
-        console.log("displaying solution");
-        solutionDisplay.innerText += solutionVal;
-    } else { //if equals hasn't been pressed store problem in problem space
-        console.log("displaying problem");
+function display () {
+    if (isEqualsPressed == false) { //problem display
         problemDisplay.innerText += value;
     }
 
-    if (value == "AC") {
-        clearDisplay();
-        console.log("cleared");
+    if (isEqualsPressed == true) { //solution display
+        solutionDisplay.innerText = solutionVal;
     }
 }
 
-//section 5
-//get everything working
+//section
+//put everything together
 
 btn.forEach(btn => {
     btn.addEventListener("click", (e) => {
         value = e.target.innerText;
-
         btnClick(value);
         display(value);
-    });
+    })
 });
-
-//section 6
-//reset everything
-
-function reset () {
-    isEqualsPressed = false;
-    isFirstVal = false;
-    isSecondVal = false;
-    isOpVal = false;
-}
-
-
-
-
-
